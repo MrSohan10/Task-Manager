dcd7d246a8e2c638426e7eff3d7b7ca8bb763de1
MrSohan10 <iamsohan11@gmail.com>
2023-11-28 09:04:30 +0600

    sharePreferences

--- lib/ui/screen/login_screen.dart
+++ lib/ui/screen/login_screen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_manager/data/model/user_model.dart';
+import 'package:task_manager/data/network_caller.dart';
+import 'package:task_manager/data/network_response.dart';
+import 'package:task_manager/ui/controller/auth_controller.dart';
 import 'package:task_manager/ui/screen/forget_password/email_verify.dart';
 import 'package:task_manager/ui/screen/main_butom_nav-screen.dart';
 import 'package:task_manager/ui/screen/sign_up_screen.dart';
 import 'package:task_manager/ui/widget/body_background.dart';
+import 'package:task_manager/ui/widget/snackbar_message.dart';
+
+import '../../data/utility.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,6 +21,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _loginInProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,104 +36,176 @@ class _LoginScreenState extends State<LoginScreen> {
             padding: const EdgeInsets.all(48),
             child: SingleChildScrollView(
               reverse: true,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Text(
-                    "Get Started With",
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(
-                    height: 32,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      labelText: "E-mail",
-                      hintText: "Enter Email",
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 80,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: "Password",
-                      hintText: "Enter Password",
+                    Text(
+                      "Get Started With",
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const MainBottomNavScreen()),
-                            (route) => false);
-                      },
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(fontSize: 16),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: "E-mail",
+                        hintText: "Enter Email",
                       ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 48,
-                  ),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const EmailVerification()));
+                      validator: (value) {
+                        if (value?.trim().isEmpty ?? true) {
+                          return "Enter Value";
+                        } else if (RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value!)) {
+                          return null;
+                        }
+                        ;
+                        return "invalid E-mail";
                       },
-                      child: const Text(
-                        "Forget Password ?",
-                        style: TextStyle(fontSize: 16),
-                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Don't have account?",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: "Password",
+                        hintText: "Enter Password",
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return "Enter Value";
+                        }
+                        if (value!.length < 6) {
+                          return "Enter password more then 6 digit";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Visibility(
+                        visible: _loginInProgress == false,
+                        replacement:
+                            const Center(child: CircularProgressIndicator()),
+                        child: ElevatedButton(
+                          onPressed: login,
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(fontSize: 16),
+                          ),
                         ),
                       ),
-                      TextButton(
+                    ),
+                    const SizedBox(
+                      height: 48,
+                    ),
+                    Center(
+                      child: TextButton(
                         onPressed: () {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => const SignUpScreen()));
+                                  builder: (context) =>
+                                      const EmailVerification()));
                         },
                         child: const Text(
-                          "Sign up",
+                          "Forget Password ?",
                           style: TextStyle(fontSize: 16),
                         ),
-                      )
-                    ],
-                  )
-                ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Don't have account?",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const SignUpScreen()));
+                          },
+                          child: const Text(
+                            "Sign up",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> login() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    _loginInProgress = true;
+    if (mounted) {
+      setState(() {});
+    }
+    NetworkResponse response = await NetworkCaller().postRequest(Urls.login,
+        body: {
+          "email": _emailController.text.trim(),
+          "password": _passwordController.text
+        });
+    _loginInProgress = false;
+    if (mounted) {
+      setState(() {});
+    }
+
+    if (response.isSuccess) {
+      await AuthController.saveInformation(response.jsonResponse['token'],
+          UserModel.fromJson(response.jsonResponse['data']));
+      if (mounted) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const MainBottomNavScreen()));
+      }
+    } else {
+      if (response.statusCode == 401) {
+        if (mounted) {
+          showSnackbar(
+              context, "Login failed! Please check email/password", true);
+        }
+      } else {
+        if (mounted) {
+          showSnackbar(context, 'Login failed! please try again', true);
+        }
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 }
--- lib/ui/screen/splash_screen.dart
+++ lib/ui/screen/splash_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_manager/ui/controller/auth_controller.dart';
 import 'package:task_manager/ui/screen/login_screen.dart';
+import 'package:task_manager/ui/screen/main_butom_nav-screen.dart';
 import 'package:task_manager/ui/widget/body_background.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,18 +14,23 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
     super.initState();
     goToLogin();
   }
 
-  void goToLogin() {
+  void goToLogin() async {
+    final bool isLogIn = await AuthController.checkAuthState();
+
     Future.delayed(const Duration(seconds: 3)).then((value) {
-      Navigator.pushAndRemoveUntil(context,
-          MaterialPageRoute(builder: (context) => const LoginScreen()), (
-              route) => false);
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+              builder: (context) => isLogIn
+                  ? const MainBottomNavScreen()
+                  : const LoginScreen ()),
+          (route) => false);
     });
   }
 
